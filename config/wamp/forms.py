018c2c1198c654fffe9471697920b90a360972fd
Sani Omotola <noble.b.sage01@gmail.com>
2025-09-21 07:24:30 +0100

    removed tracking files

--- config/wamp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import SampleModel
-
-class SampleModelForm(forms.ModelForm):
-    class Meta:
-        model = SampleModel
-        fields = ['name', 'description', 'age', 'email', 'height', 'weight', 'is_active', 'occupation']
